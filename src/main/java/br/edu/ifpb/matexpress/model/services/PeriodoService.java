f6f88ee0c5d41461d62e41aa9b66a17af617cef0
renataalbq <renataalbq1@gmail.com>
2023-04-08 09:08:15 -0300

    formata data na tabela / criado validaForm no service

--- src/main/java/br/edu/ifpb/matexpress/model/services/PeriodoService.java
+++ src/main/java/br/edu/ifpb/matexpress/model/services/PeriodoService.java
@@ -4,8 +4,11 @@ import br.edu.ifpb.matexpress.model.entities.PeriodoLetivo;
 import br.edu.ifpb.matexpress.model.repositories.PeriodoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +20,7 @@ public class PeriodoService {
 
     @Transactional
     public void cadastrarPeriodo(PeriodoLetivo newPeriodo) {
+        validarForm(newPeriodo);
         this.periodoRepository.save(newPeriodo);
     }
 
@@ -31,7 +35,14 @@ public class PeriodoService {
     public void deletarPorId(Long idPeriodo){
         PeriodoLetivo periodoBanco = this.pesquisarPeriodoPorId(idPeriodo);
         this.periodoRepository.delete(periodoBanco);
+    }
 
-
+    private void validarForm(PeriodoLetivo periodo) {
+        if (periodo.getPeriodo() == null || periodo.getInicio() == null || periodo.getFim() == null || periodo.getAno() == null) {
+            throw new IllegalArgumentException("Campos não podem ser nulos");
+        }
+        else if (periodo.getInicio().isAfter(periodo.getFim())) {
+            throw new IllegalArgumentException("Data inválida");
+        }
     }
 }
