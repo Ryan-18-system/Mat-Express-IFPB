51d7f33979f25f5561ca02ed4a0ab08a231e1ac7
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2023-04-29 12:26:41 -0300

    Validação de instituição

--- src/main/java/br/edu/ifpb/matexpress/controllers/InstituicaoController.java
+++ src/main/java/br/edu/ifpb/matexpress/controllers/InstituicaoController.java
@@ -1,17 +1,23 @@
 package br.edu.ifpb.matexpress.controllers;
 
-import br.edu.ifpb.matexpress.model.entities.Instituicao;
-import br.edu.ifpb.matexpress.model.entities.PeriodoLetivo;
-import br.edu.ifpb.matexpress.model.services.InstituicaoService;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import br.edu.ifpb.matexpress.model.entities.Instituicao;
+import br.edu.ifpb.matexpress.model.entities.PeriodoLetivo;
+import br.edu.ifpb.matexpress.model.services.InstituicaoService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/instituicoes")
@@ -19,7 +25,6 @@ public class InstituicaoController {
     @Autowired
     private InstituicaoService instituicaoService;
     String mensagem;
-
 
     @GetMapping("/")
     public ModelAndView formInstituicoes(Instituicao instituicao, ModelAndView modelAndView) {
@@ -29,7 +34,12 @@ public class InstituicaoController {
     }
 
     @PostMapping("**/cadastrar")
-    public ModelAndView cadastrarInstituicao(Instituicao instituicao, ModelAndView modelAndView, RedirectAttributes redirectAttributes) {
+    public ModelAndView cadastrarInstituicao(@Valid Instituicao instituicao, BindingResult validation,
+            ModelAndView modelAndView, RedirectAttributes redirectAttributes) {
+        if (validation.hasErrors()) {
+            modelAndView.setViewName("instituicoes/form");
+            return modelAndView;
+        }
         mensagem = this.instituicaoService.cadastrarInstituicao(instituicao);
         modelAndView.setViewName("redirect:/matexpress/instituicoes");
         redirectAttributes.addFlashAttribute("mensagem", mensagem);
@@ -45,9 +55,8 @@ public class InstituicaoController {
 
     @GetMapping("/editarinstituicao/{idInstituicao}")
     public ModelAndView editarInstituicao(ModelAndView modelAndView,
-                                          @PathVariable("idInstituicao") Long idInstituicao
-    ) {
-        List<PeriodoLetivo>  periodos = new ArrayList<>();
+            @PathVariable("idInstituicao") Long idInstituicao) {
+        List<PeriodoLetivo> periodos = new ArrayList<>();
         periodos.addAll(this.instituicaoService.listarPeriodosCadastrados());
         periodos.addAll(this.instituicaoService.listarPeriodosDaInstituicao(idInstituicao));
         modelAndView.setViewName("instituicoes/form");
@@ -58,8 +67,8 @@ public class InstituicaoController {
 
     @GetMapping("/removerinstituicao/{idInstituicao}")
     public ModelAndView removerInstituicao(ModelAndView modelAndView,
-                                           @PathVariable("idInstituicao") Long idInstituicao,
-                                           RedirectAttributes redirectAttributes) {
+            @PathVariable("idInstituicao") Long idInstituicao,
+            RedirectAttributes redirectAttributes) {
         mensagem = this.instituicaoService.deletarPorId(idInstituicao);
         redirectAttributes.addFlashAttribute("mensagem", mensagem);
         modelAndView.setViewName("redirect:/matexpress/instituicoes");
