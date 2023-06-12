029af2bb9a51094de31e536d0448c67cef432ece
renataalbq <renataalbq1@gmail.com>
2023-06-12 17:09:17 -0300

    authentication

--- src/main/java/br/edu/ifpb/matexpress/controllers/AuthController.java
+++ src/main/java/br/edu/ifpb/matexpress/controllers/AuthController.java
@@ -1,11 +1,15 @@
 package br.edu.ifpb.matexpress.controllers;
 
 import br.edu.ifpb.matexpress.model.entities.Estudante;
+import br.edu.ifpb.matexpress.model.repositories.EstudanteRepository;
+import br.edu.ifpb.matexpress.util.PasswordUtil;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
@@ -26,3 +30,4 @@ public class AuthController {
         return mav;
     }
 }
+
